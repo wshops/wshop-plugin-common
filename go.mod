8b5e32ade025bf3380eb72102f1c8c2ae277ba33
Tony An <anxuanzi@sh-c.cn>
2022-11-30 00:06:11 -0600

    regenerate code

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/wshops/wshop-plugin-common
 go 1.19
 
 require (
-	github.com/knqyf263/go-plugin v0.3.0
+	github.com/knqyf263/go-plugin v0.3.1-0.20221129003012-36abc427c0eb
 	github.com/tetratelabs/wazero v1.0.0-pre.3
 	google.golang.org/protobuf v1.28.1
 )
--- pkg/pcaptcha/CaptchaPlugin_host.pb.go
+++ pkg/pcaptcha/CaptchaPlugin_host.pb.go
@@ -163,23 +163,25 @@ func (p *captchaPlugin) GetPluginInfo(ctx context.Context, request emptypb.Empty
 		return response, err
 	}
 	dataSize := uint64(len(data))
-	if dataSize == 0 {
-		return response, nil
-	}
-	results, err := p.malloc.Call(ctx, dataSize)
-	if err != nil {
-		return response, err
+
+	var dataPtr uint64
+	// If the input data is not empty, we must allocate the in-Wasm memory to store it, and pass to the plugin.
+	if dataSize != 0 {
+		results, err := p.malloc.Call(ctx, dataSize)
+		if err != nil {
+			return response, err
+		}
+		dataPtr = results[0]
+		// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
+		// So, we have to free it when finished
+		defer p.free.Call(ctx, dataPtr)
+
+		// The pointer is a linear memory offset, which is where we write the name.
+		if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
+			return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
+		}
 	}
 
-	dataPtr := results[0]
-	// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
-	// So, we have to free it when finished
-	defer p.free.Call(ctx, dataPtr)
-
-	// The pointer is a linear memory offset, which is where we write the name.
-	if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
-		return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
-	}
 	ptrSize, err := p.getplugininfo.Call(ctx, dataPtr, dataSize)
 	if err != nil {
 		return response, err
@@ -208,23 +210,25 @@ func (p *captchaPlugin) VerifyCaptcha(ctx context.Context, request VerifyCaptcha
 		return response, err
 	}
 	dataSize := uint64(len(data))
-	if dataSize == 0 {
-		return response, nil
-	}
-	results, err := p.malloc.Call(ctx, dataSize)
-	if err != nil {
-		return response, err
+
+	var dataPtr uint64
+	// If the input data is not empty, we must allocate the in-Wasm memory to store it, and pass to the plugin.
+	if dataSize != 0 {
+		results, err := p.malloc.Call(ctx, dataSize)
+		if err != nil {
+			return response, err
+		}
+		dataPtr = results[0]
+		// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
+		// So, we have to free it when finished
+		defer p.free.Call(ctx, dataPtr)
+
+		// The pointer is a linear memory offset, which is where we write the name.
+		if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
+			return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
+		}
 	}
 
-	dataPtr := results[0]
-	// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
-	// So, we have to free it when finished
-	defer p.free.Call(ctx, dataPtr)
-
-	// The pointer is a linear memory offset, which is where we write the name.
-	if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
-		return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
-	}
 	ptrSize, err := p.verifycaptcha.Call(ctx, dataPtr, dataSize)
 	if err != nil {
 		return response, err
@@ -253,23 +257,25 @@ func (p *captchaPlugin) GetCustomHtmlInputField(ctx context.Context, request emp
 		return response, err
 	}
 	dataSize := uint64(len(data))
-	if dataSize == 0 {
-		return response, nil
-	}
-	results, err := p.malloc.Call(ctx, dataSize)
-	if err != nil {
-		return response, err
+
+	var dataPtr uint64
+	// If the input data is not empty, we must allocate the in-Wasm memory to store it, and pass to the plugin.
+	if dataSize != 0 {
+		results, err := p.malloc.Call(ctx, dataSize)
+		if err != nil {
+			return response, err
+		}
+		dataPtr = results[0]
+		// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
+		// So, we have to free it when finished
+		defer p.free.Call(ctx, dataPtr)
+
+		// The pointer is a linear memory offset, which is where we write the name.
+		if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
+			return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
+		}
 	}
 
-	dataPtr := results[0]
-	// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
-	// So, we have to free it when finished
-	defer p.free.Call(ctx, dataPtr)
-
-	// The pointer is a linear memory offset, which is where we write the name.
-	if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
-		return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
-	}
 	ptrSize, err := p.getcustomhtmlinputfield.Call(ctx, dataPtr, dataSize)
 	if err != nil {
 		return response, err
@@ -298,23 +304,25 @@ func (p *captchaPlugin) GetCustomHtmlHead(ctx context.Context, request emptypb.E
 		return response, err
 	}
 	dataSize := uint64(len(data))
-	if dataSize == 0 {
-		return response, nil
-	}
-	results, err := p.malloc.Call(ctx, dataSize)
-	if err != nil {
-		return response, err
+
+	var dataPtr uint64
+	// If the input data is not empty, we must allocate the in-Wasm memory to store it, and pass to the plugin.
+	if dataSize != 0 {
+		results, err := p.malloc.Call(ctx, dataSize)
+		if err != nil {
+			return response, err
+		}
+		dataPtr = results[0]
+		// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
+		// So, we have to free it when finished
+		defer p.free.Call(ctx, dataPtr)
+
+		// The pointer is a linear memory offset, which is where we write the name.
+		if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
+			return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
+		}
 	}
 
-	dataPtr := results[0]
-	// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
-	// So, we have to free it when finished
-	defer p.free.Call(ctx, dataPtr)
-
-	// The pointer is a linear memory offset, which is where we write the name.
-	if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
-		return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
-	}
 	ptrSize, err := p.getcustomhtmlhead.Call(ctx, dataPtr, dataSize)
 	if err != nil {
 		return response, err
@@ -343,23 +351,25 @@ func (p *captchaPlugin) GetCustomHtmlBodyEnd(ctx context.Context, request emptyp
 		return response, err
 	}
 	dataSize := uint64(len(data))
-	if dataSize == 0 {
-		return response, nil
-	}
-	results, err := p.malloc.Call(ctx, dataSize)
-	if err != nil {
-		return response, err
+
+	var dataPtr uint64
+	// If the input data is not empty, we must allocate the in-Wasm memory to store it, and pass to the plugin.
+	if dataSize != 0 {
+		results, err := p.malloc.Call(ctx, dataSize)
+		if err != nil {
+			return response, err
+		}
+		dataPtr = results[0]
+		// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
+		// So, we have to free it when finished
+		defer p.free.Call(ctx, dataPtr)
+
+		// The pointer is a linear memory offset, which is where we write the name.
+		if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
+			return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
+		}
 	}
 
-	dataPtr := results[0]
-	// This pointer is managed by TinyGo, but TinyGo is unaware of external usage.
-	// So, we have to free it when finished
-	defer p.free.Call(ctx, dataPtr)
-
-	// The pointer is a linear memory offset, which is where we write the name.
-	if !p.module.Memory().Write(ctx, uint32(dataPtr), data) {
-		return response, fmt.Errorf("Memory.Write(%d, %d) out of range of memory size %d", dataPtr, dataSize, p.module.Memory().Size(ctx))
-	}
 	ptrSize, err := p.getcustomhtmlbodyend.Call(ctx, dataPtr, dataSize)
 	if err != nil {
 		return response, err
